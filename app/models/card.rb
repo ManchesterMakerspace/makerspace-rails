1c526121997b1f086d09d65df15e4abed745fa2a
lynch16 <will.lynch91@gmail.com>
2017-06-10 14:53:04 -0400

    can report cards as lost or stole and view their current status

--- app/models/card.rb
+++ app/models/card.rb
@@ -6,7 +6,9 @@ class Card
   field :validity, type: String #Member's Status
   attr_accessor :card_location
 
-  before_save :load_member_attribtues
+  before_create :load_member_attribtues
+  before_update :update_member_attributes
+  after_save :check_validity, if: :card_location
 
   belongs_to :member
 
@@ -14,12 +16,20 @@ class Card
     self.holder = self.member.fullname
     self.member_id = self.member.id.to_s
     self.expiry = self.member.expirationTime
-    self.validity = self.check_validity
+    self.validity = self.check_validity;
+  end
+
+  def update_member_attributes
+    self.holder = self.member.fullname
+    self.member_id = self.member.id.to_s
+    self.expiry = self.member.expirationTime
   end
 
   def check_validity
     if (!!self.card_location)
       self.validity = self.card_location
+      self.card_location = nil
+      self.save
     else (self.member.status == 'activeMember' || self.member.status == 'nonMember' || self.member.status == 'revoked')
       if (self.member.membership_status != 'expired')
         self.validity = self.member.status
