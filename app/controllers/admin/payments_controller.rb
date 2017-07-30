7affb64cfeb9ab8421736b22e0b21186485388d9
lynch16 <will.lynch91@gmail.com>
2017-07-29 22:33:35 -0400

    stub payments index pg

--- app/controllers/admin/payments_controller.rb
+++ app/controllers/admin/payments_controller.rb
@@ -1,21 +1,25 @@
 class Admin::PaymentsController < ApplicationController
 
   def index
-    @payments = Payment.prepare_updates
-  end
-
-  def new
-    @payment = Payment.new
-  end
-
-  def create
-    @payment = Payment.new(payment_params)
-    if @payment.save
-      redirect_to payments_path, notice: 'Success'
-    else
-      render :new, alert: 'Failure'
+    @payments = Payment.all.collect do |p|
+      p.member = p.find_member
+      p
     end
+    @members = Member.all
   end
+
+  # def new
+  #   @payment = Payment.new
+  # end
+  #
+  # def create
+  #   @payment = Payment.new(payment_params)
+  #   if @payment.save
+  #     redirect_to payments_path, notice: 'Success'
+  #   else
+  #     render :new, alert: 'Failure'
+  #   end
+  # end
 
   private
   def payment_params
