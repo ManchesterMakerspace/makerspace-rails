24c4277490df53c69c173c0a88954f300cde29fe
lynch16 <will.lynch91@gmail.com>
2017-08-05 22:42:23 -0400

    point db at mlab

--- config/application.rb
+++ config/application.rb
@@ -2,12 +2,12 @@ require_relative 'boot'
 
 
 # Pick the frameworks you want:
-require "active_record/railtie"
-require "action_controller/railtie"
+# require "active_record/railtie"
 require "active_model/railtie"
 require "active_job/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+require "action_controller/railtie"
 require "action_mailer/railtie"
 require "sprockets/railtie"
 require "rails/test_unit/railtie"
