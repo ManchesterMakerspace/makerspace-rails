d782e03d6a5a4e4469e465825b1729325a0b2b5a
Will Lynch <will.lynch@oracle.com>
2018-06-21 16:28:51 -0400

    use main app to generate tester

--- config/application.rb
+++ config/application.rb
@@ -28,7 +28,6 @@ Mongoid.load!('config/mongoid.yml')
 
 module MemberInterface
   class Application < Rails::Application
-    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
     config.to_prepare do
       DeviseController.respond_to :html, :json
     end
