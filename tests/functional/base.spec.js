bf0e5e1729e1977f8b6cbca54e4876f69059855f
Will Lynch <will@awato.org>
2018-09-27 08:36:16 -0400

    use own env wrapper

--- tests/functional/base.spec.js
+++ tests/functional/base.spec.js
@@ -1,35 +1,20 @@
-const mockServerClient = require('mockserver-client').mockServerClient;
+require("./locators");
 
-const rootURL = 'http://www.localhost:3002/'
-const waitUntilTime = 20000
-let driver;
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000 * 60 * 5
-
-async function getElementById(id) {
-  const el = await browser.wait(until.elementLocated(by.id(id)), waitUntilTime)
-  return await browser.wait(until.elementIsVisible(el), waitUntilTime)
-}
-
-async function getElementByCss(css) {
-  const el = await browser.wait(until.elementLocated(by.css(css)), waitUntilTime)
-  return await browser.wait(until.elementIsVisible(el), waitUntilTime)
-}
+const mockserver = require('mockserver-client').mockServerClient("localhost", 1080);
+const rootURL = 'http://www.localhost:3002/';
 
 it('initialises the context', async () => {
-  await browser.get(rootURL)
-})
+  await browser.manage().window().setPosition(0, 0);
+  await browser.manage().window().setSize(1280, 1024);
+  await browser.get(rootURL);
+});
 
 it('should load the landing page', async () => {
   const el = await getElementByCss('[id="landing-page-graphic"]');
   expect(await el.getText()).toEqual("Manchester Makerspace");
-})
+});
 
-let mockserver;
 describe("API Mocking", () => {
-  beforeEach(() => {
-    mockserver = mockServerClient("localhost", 1080)
-  });
-
   it("Mocks sign in request", async () => {
     await mockserver.mockAnyResponse({
         "httpRequest": {
@@ -66,9 +51,8 @@ describe("API Mocking", () => {
       }
     })
     await browser.get(rootURL);
-    const el = await getElementByCss('[id="landing-page-graphic"]');
-    browser.wait(() => {
-      return el.isDisplayed().then((d) => !d);
-    });
+    waitForPageChange()
+    const url = await browser.getCurrentUrl();
+    expect(url).not.toEqual(rootURL);    
   });
 })
