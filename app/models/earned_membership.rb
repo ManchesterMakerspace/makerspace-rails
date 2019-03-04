49273ee9a8374fe5ee06cd03462e6a4a22a74a05
Will Lynch <will.lynch@oracle.com>
2019-03-04 14:45:00 -0500

    prevent earned memberships for subscription members

--- app/models/earned_membership.rb
+++ app/models/earned_membership.rb
@@ -9,6 +9,7 @@ class EarnedMembership
   has_many :reports, class_name: 'EarnedMembership::Report'
   accepts_nested_attributes_for :requirements, reject_if: :all_blank, allow_destroy: true
   validate :one_to_one
+  validate :existing_subscription, on: :create
 
   def has_outstanding_requirements
     outstanding_requirements = current_requirements.select do |requirement|
@@ -24,8 +25,15 @@ class EarnedMembership
   private
   def one_to_one
     member_memberships = EarnedMembership.where(member_id: self.member_id)
+    # memberships exist and aren't this one
     if member_memberships.size > 0 && !(member_memberships.size == 1 && member_memberships.first.id == self.id)
       errors.add(:member, "Earned membership already exists for member #{member_memberships.first.member.fullname}")
+    end
+  end
+
+  def existing_subscription
+    if self.member.subscription || self.member.subscription_id
+      errors.add(:member, "#{self.member.fullname} is still on subscription. Must cancel subscription first")
     end
   end
 end
