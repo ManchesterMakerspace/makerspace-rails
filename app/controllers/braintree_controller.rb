20d4182326c0d242db4401b86ec33fc69e1cffff
Will Lynch <will.lynch@oracle.com>
2019-03-28 18:39:20 -0400

    add mailers where needed

--- app/controllers/braintree_controller.rb
+++ app/controllers/braintree_controller.rb
@@ -1,11 +1,10 @@
 class BraintreeController < ApplicationController
   include BraintreeGateway
-  include Service::SlackConnector
   before_action :validate_notification
 
   def webhooks
     if @notification.kind == ::Braintree::WebhookNotification::Kind::Check
-      send_slack_message("Braintree Webhook test notification succeeded!")
+      @messages.push("Braintree Webhook test notification succeeded!")
     else
       ::BraintreeService::Notification.process(@gateway, @notification)
     end
