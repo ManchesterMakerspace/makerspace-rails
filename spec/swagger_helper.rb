69eec01a486d0722bf4dc90d8648fff9dad408f6
Will Lynch <will.lynch91@gmail.com>
2019-06-30 19:35:43 -0400

    add definitions

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -1,4 +1,6 @@
 require 'rails_helper'
+# TODO: The oneOf definitions don't work
+
 
 RSpec.configure do |config|
   # Specify a root folder where Swagger JSON files are generated
@@ -20,6 +22,59 @@ RSpec.configure do |config|
         version: 'v1'
       },
       definitions: {
+        Card: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            holder: { type: :string },
+            expiry: { type: :number },
+            validity: { type: :string },
+            uid: { type: :string },
+          }
+        },
+
+        CreditCard: {
+          type: :object,
+          properties: {
+            customerId: { type: :string },
+            imageUrl: { type: :string },
+            subscriptions: {
+              type: :array,
+              items: { '$ref' => '#/definitions/Subscription' }
+            },
+            cardType: { type: :string },
+            expirationMonth: { type: :string },
+            expirationYear: { type: :string },
+            expirationDate: { type: :string },
+            last4: { type: :string },
+          }
+        },
+
+        Discount: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            name: { type: :string },
+            description: { type: :string },
+            amount: { type: :string }
+          }
+        },
+
+        EarnedMembership: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            memberId: { type: :string },
+            memberName: { type: :string },
+            memberStatus: { type: :string },
+            memberExpiration: { type: :number },
+            requirements: {
+              type: :array,
+              items: { '$ref' => '#/definitions/Requirement' }
+            }
+          }
+        },
+
         error: {
           type: :object,
           properties: {
@@ -28,7 +83,51 @@ RSpec.configure do |config|
             status: { type: :string },
           }
         },
-        member: {
+
+        Invoice: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            name: { type: :string },
+            description: { type: :string },
+            settled: { type: :boolean },
+            pastDue: { type: :boolean },
+            createdAt: { type: :date },
+            dueDate: { type: :date },
+            amount: { type: :string },
+            subscriptionId: { type: :string },
+            planId: { type: :string },
+            resourceClass: { type: :string },
+            resourceId: { type: :string },
+            quantity: { type: :number },
+            discountId: { type: :string },
+            memberName: { type: :string },
+            resource: { 
+              type: :object,
+              oneOf: [
+                '$ref' => '#/definitions/Member',
+                '$ref' => '#/definitions/Rental'
+              ]
+            }
+          }
+        },
+
+        InvoiceOption: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            name: { type: :string },
+            description: { type: :string },
+            amount: { type: :string },
+            planId: { type: :string },
+            resourceClass: { type: :string },
+            quantity: { type: :number },
+            discountId: { type: :string },
+            disabled: { type: :boolean },
+          }
+        },
+
+        Member: {
           type: :object,
           properties: {
             id: { type: :string },
@@ -44,7 +143,169 @@ RSpec.configure do |config|
             customerId: { type: :string },
             earnedMembershipId: { type: :string },
           }
-        }
+        },
+        
+        PayPalAccount: {
+          type: :object,
+          properties: {
+            customerId: { type: :string },
+            imageUrl: { type: :string },
+            subscriptions: {
+              type: :array,
+              items: { '$ref' => '#/definitions/Subscription' }
+            },
+            email: { type: :string },
+          }
+        },
+
+        # permission: {
+        #   type: :object,
+        #   properties: {
+            
+        #   }
+        # },
+
+        Plan: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            name: { type: :string },
+            description: { type: :string },
+            amount: { type: :string },
+            billingFrequency: { type: :string },
+            discounts: {
+              type: :array,
+              items: {
+                properties: {
+                  id: { type: :string },
+                  name: { type: :string },
+                  description: { type: :string },
+                  amount: { type: :string },
+                }
+              }
+            }
+          }
+        },
+
+        Requirement: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            earnedMembershipId: { type: :string },
+            name: { type: :string },
+            rolloverLimit: { type: :number },
+            termLength: { type: :number },
+            targetCount: { type: :number },
+            strict: { type: :boolean },
+            currentCount: { type: :number },
+            termStartDate: { type: :date },
+            termEndDate: { type: :date },
+            termId: { type: :string },
+            satisfied: { type: :boolean }
+          }
+        },
+
+        ReportRequirement: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            requirementId: { type: :string },
+            reportedCount: { type: :number },
+            appliedCount: { type: :number },
+            currentCount: { type: :number },
+            memberIds: {
+              type: :array,
+              items: {
+                type: :string
+              }
+            },
+            termStartDate: { type: :date },
+            termEndDate: { type: :date },
+            satisfied: { type: :boolean }
+          }
+        },
+
+        Report: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            date: { type: :date },
+            earnedMembershipId: { type: :string },
+            reportRequirements: {
+              type: :array,
+              items: { '$ref' => '#/definitions/EarnedMembership' }
+            }
+          }
+        },
+
+        Rental: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            number: { type: :string },
+            description: { type: :string },
+            memberName: { type: :string },
+            memberId: { type: :string },
+            expiration: { type: :date }
+          }
+        },
+
+        Subscription: {
+          type: :object,
+          properties: {
+            id: { type: :string },
+            planId: { type: :string },
+            status: { type: :string },
+            amount: { type: :string },
+            failureCount: { type: :number },
+            daysPastDue: { type: :number },
+            billingDayOfMonth: { type: :string },
+            firstBillingDate: { type: :date },
+            nextBillingDate: { type: :date },
+            memberId: { type: :string },
+            memberName: { type: :string },
+            resourceClass: { type: :string },
+            resourceId: { type: :string },
+            paymentMethodToken: { type: :string }
+          }
+        },
+       
+
+        Transaction: {
+          type: :object,
+          properties: {
+            createdAt: { type: :date },
+            customerDetails: { type: :object },
+            disputes: { type: :array },
+            discountAmount: { type: :string },
+            discounts: { type: :array },
+            gatewayRejectionReason: { type: :string },
+            status: { type: :string },
+            id: { type: :string },
+            planId: { type: :string },
+            recurring: { type: :boolean },
+            refundIds: { type: :array, items: { type: :string } },
+            refundedTransactionId: { type: :string },
+            subscriptionDetails: { 
+              type: :object,
+              properties: {
+                billingPeriodStartDate: { type: :date },
+                billingPeriodEndDate: { type: :date },
+              }
+            },
+            subscriptionId: { type: :string },
+            amount: { type: :string },
+            memberId: { type: :string },
+            memberName: { type: :string },
+            paymentMethodDetails: {
+              oneOf: [
+                '$ref' => '#/definitions/CreditCard',
+                '$ref' => '#/definitions/PayPalAccount',
+              ]
+            }
+          }
+        },
+
       }
     }
   }
