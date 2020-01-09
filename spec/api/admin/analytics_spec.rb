3ad7988c540bd30eeeabb0cfc1d04c06f5055d1a
Will Lynch <will.lynch91@gmail.com>
2020-01-08 19:49:21 -0500

    add analytics route #minor (#86)

---
+++ spec/api/admin/analytics_spec.rb
@@ -0,0 +1,37 @@
+require 'swagger_helper'
+
+describe 'Analytics API', type: :request do
+  let(:admin) { create(:member, :admin) }
+
+  path '/admin/analytics' do
+    get 'Lists analytic counts' do
+      tags 'Analytics'
+      operationId "listAnalytics"
+
+
+      response '200', 'analytics read' do
+        before { sign_in admin }
+
+        schema type: :object,
+        properties: {
+          analytics: {
+            type: :object,
+            properties: {
+              totalMembers: { type: :number },
+              newMemebrs: { type: :number },
+              subscribedMembers: { type: :number },
+              pastDueInvoices: { type: :number },
+              refundsPending: { type: :number },
+            }
+          }
+        },
+        required: [ 'analytics' ]
+
+        let(:rentals) { create_list(:rental) }
+        let(:members) { create_list(:member) }
+        let(:invoices) { create_list(:invoice) }
+        run_test!
+      end
+    end
+  end
+end
