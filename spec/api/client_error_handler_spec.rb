ef04964fea843e346cf27b23c6852d02b4885418
Will Lynch <will.lynch91@gmail.com>
2020-03-07 22:38:31 -0500

    add path for sending client errors #minor

---
+++ spec/api/client_error_handler_spec.rb
@@ -0,0 +1,33 @@
+require 'swagger_helper'
+
+describe 'ClientErrorHandler API', type: :request do
+  path '/client_error_handler' do 
+    post 'Sends a slack message' do 
+      tags 'ClientErrorHandler'
+      operationId 'message'
+
+      parameter name: :messageDetails, in: :body, schema: {
+        type: :object,
+        properties: {
+          notification: {
+            type: :object,
+            properties: {
+              message: {
+                type: :string
+              }
+            }
+          }
+        }
+      }
+
+      response '204', 'Message sent' do 
+        let(:messageDetails) {{
+          notification: {
+            message: "Test message"
+          }
+        }}
+        run_test!
+      end
+    end
+  end
+end
