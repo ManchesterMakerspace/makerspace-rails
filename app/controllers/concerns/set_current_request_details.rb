ec309efd9d968904d33ce9978dbdb26a1bf48789
Will Lynch <will.lynch91@gmail.com>
2021-02-22 19:48:03 -0500

    include method, url and params in error handler messaging

--- app/controllers/concerns/set_current_request_details.rb
+++ app/controllers/concerns/set_current_request_details.rb
@@ -3,6 +3,9 @@ module SetCurrentRequestDetails
 
   included do
     before_action do
+      Current.url = request.url
+      Current.method = request.method
+      Current.params = request.params
       Current.request_id = request.uuid
       Current.user_agent = request.user_agent
       Current.ip_address = request.ip
--- app/models/current.rb
+++ app/models/current.rb
@@ -1,4 +1,4 @@
 # A model that is used to track properties without drilling them through fns
 class Current < ActiveSupport::CurrentAttributes
-    attribute :request_id, :user_agent, :ip_address
+    attribute :request_id, :user_agent, :ip_address, :url, :method, :params
 end
