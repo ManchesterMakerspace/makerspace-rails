df7f11f242cf6b2f56437de58b2bb24cdda64e34
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-03-17 15:13:28 -0400

    Remove admin signin #patch

--- spec/api/members_spec.rb
+++ spec/api/members_spec.rb
@@ -11,8 +11,7 @@ describe 'Members API', type: :request do
       parameter name: :currentMembers, in: :query, type: :boolean, required: false
       parameter name: :search, in: :query, type: :string, required: false
 
-      response '200', 'Members found' do
-        before { sign_in admin }
+      response '200', 'Members found' do 
         let(:members) { create_list(:member) }
         before { sign_in create(:member) }
 
