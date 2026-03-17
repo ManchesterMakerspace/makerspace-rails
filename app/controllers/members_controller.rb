11ab4f056450708ad07c0e902b33949dbc527cd8
Kevin Kadow <99362831+Kv603@users.noreply.github.com>
2026-03-17 14:02:48 -0400

    Refactor member index query logic

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,29 +5,18 @@ class MembersController < AuthenticationController
     def index
       base_query = Member.includes(:access_cards).includes(:earned_membership)
       # Limit index to only current members unless authorized and requesting full records
-      #current_member.email
       #raise ::Error::Forbidden.new unless is_admin?
       if !is_admin? || to_bool(search_params[:current_members])
         # Include unset or expired within grace period
-        if !is_admin?
-          search = base_query.where({ email : current_member.email })
-        else
-           search = base_query.where({
+        search = base_query.where({
           :$or => [
             { :expirationTime.gte => ((Time.now + 3.days).strftime('%s').to_i * 1000) },
             {  expirationTime: nil }
           ]
-           })
-        end
+        })
       else
-        if !is_admin?
-          search = base_query.where({ email : current_member.email })
-        else
-           search = Mongoid::Criteria.new(base_query)
-        end
+        search = Mongoid::Criteria.new(base_query)
       end
-      
-
       @members = query_resource(search)
 
       return render_with_total_items(@members, { each_serializer: MemberSummarySerializer, adapter: :attributes })
